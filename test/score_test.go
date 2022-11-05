32af218043625b626aaf9129a4edb09fbc9a74b2
zjm <1076115376@qq.com>
2022-11-05 11:52:25 +0800

    parameter annotation

--- test/score_test.go
+++ test/score_test.go
@@ -7,6 +7,10 @@ import (
 
 var client sdk.CalculateEvaluate
 
+// PredPath 用户上传的result.txt
+// TruePath 标准答案的result.txt
+// cls 比赛的类别数
+// pos 类别索引标签的起始位
 func TestEvaluate(t *testing.T) {
 	err, res := client.Evaluate(&sdk.Calculate{
 		PredPath: "xihe-obj/competitions/昇思AI挑战赛-多类别图像分类/submit_result/s9qfqri3zpc8j2x7_1/result_example_5120-2022-8-8-15-3-16.txt",
@@ -21,6 +25,7 @@ func TestEvaluate(t *testing.T) {
 	t.Log(res)
 }
 
+// UserResult 存有1000张图片的zip文件
 func TestCalculate(t *testing.T) {
 	err := client.Calculate(&sdk.Calculate{
 		UserResult: "xihe-obj/competitions/昇思AI挑战赛-艺术家画作风格迁移/submit_result/victor_1/result",
@@ -31,5 +36,5 @@ func TestCalculate(t *testing.T) {
 }
 
 func TestMain(t *testing.M) {
-	client = sdk.NewCalculateEvaluate("http://192.168.1.218:8000")
+	client = sdk.NewCalculateEvaluate("https://xihescript.test.osinfra.cn")
 }
